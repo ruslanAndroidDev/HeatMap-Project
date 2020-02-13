96a12b1a58006c7241bd2e95b026b0bba624c8fe
ruslanAndroidDev <yesinruslan@gmail.com>
2020-02-13 20:36:53 +0200

    fix small bug

--- app/src/main/java/ua/rdev/tripsmap/MainActivity.kt
+++ app/src/main/java/ua/rdev/tripsmap/MainActivity.kt
@@ -89,106 +89,113 @@ class MainActivity : AppCompatActivity() {
 
 
     fun addHeatmapLayer(@NonNull loadedMapStyle: Style) {
-        var layer = HeatmapLayer(HEATMAP_LAYER_ID, DATA_SOURCE_ID)
-        layer.maxZoom = 15f
-        layer.sourceLayer = HEATMAP_LAYER_SOURCE
-        layer.setProperties(
+        var layer = loadedMapStyle.getLayer(HEATMAP_LAYER_ID)
+        if (layer == null) {
+            layer = HeatmapLayer(HEATMAP_LAYER_ID, DATA_SOURCE_ID)
+            layer.maxZoom = 15f
+            layer.sourceLayer = HEATMAP_LAYER_SOURCE
+            layer.setProperties(
 
 // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
 // Begin color ramp at 0-stop with a 0-transparency color
 // to create a blur-like effect.
-            heatmapColor(
-                interpolate(
-                    linear(), heatmapDensity(),
-                    literal(0), rgba(33, 102, 172, 0),
-                    literal(0.2), rgb(103, 169, 207),
-                    literal(0.4), rgb(209, 229, 240),
-                    literal(0.6), rgb(253, 219, 199),
-                    literal(0.8), rgb(239, 138, 98),
-                    literal(1), rgb(178, 24, 43)
-                )
-            ),
+                heatmapColor(
+                    interpolate(
+                        linear(), heatmapDensity(),
+                        literal(0), rgba(33, 102, 172, 0),
+                        literal(0.2), rgb(103, 169, 207),
+                        literal(0.4), rgb(209, 229, 240),
+                        literal(0.6), rgb(253, 219, 199),
+                        literal(0.8), rgb(239, 138, 98),
+                        literal(1), rgb(178, 24, 43)
+                    )
+                ),
 
 // Increase the heatmap weight based on frequency and property magnitude
 
 // Increase the heatmap color weight weight by zoom level
 // heatmap-intensity is a multiplier on top of heatmap-weight
-            heatmapIntensity(
-                interpolate(
-                    linear(), zoom(),
-                    stop(0, 1),
-                    stop(15, 3)
-                )
-            ),
+                heatmapIntensity(
+                    interpolate(
+                        linear(), zoom(),
+                        stop(0, 1),
+                        stop(15, 3)
+                    )
+                ),
 
 // Adjust the heatmap radius by zoom level
-            heatmapRadius(
-                interpolate(
-                    linear(), zoom(),
-                    stop(0, 2),
-                    stop(15, 30)
-                )
-            ),
+                heatmapRadius(
+                    interpolate(
+                        linear(), zoom(),
+                        stop(0, 2),
+                        stop(15, 30)
+                    )
+                ),
 
 // Transition from heatmap to circle layer by zoom level
-            heatmapOpacity(
-                interpolate(
-                    linear(), zoom(),
-                    stop(7, 1),
-                    stop(15, 0)
+                heatmapOpacity(
+                    interpolate(
+                        linear(), zoom(),
+                        stop(7, 1),
+                        stop(15, 0)
+                    )
                 )
             )
-        )
 
-        loadedMapStyle.addLayerAbove(layer, "waterway-label")
+            loadedMapStyle.addLayerAbove(layer, "waterway-label")
+        }
     }
 
     fun addCircleLayer(@NonNull loadedMapStyle: Style) {
-        var circleLayer = CircleLayer(CIRCLE_LAYER_ID, DATA_SOURCE_ID)
-        circleLayer.setProperties(
+        var circleLayer = loadedMapStyle.getLayer(CIRCLE_LAYER_ID)
+        if (circleLayer == null) {
+
+            circleLayer = CircleLayer(CIRCLE_LAYER_ID, DATA_SOURCE_ID)
+            circleLayer.setProperties(
 
 // Size circle radius by earthquake magnitude and zoom level
-            circleRadius(
-                interpolate(
-                    linear(), zoom(),
-                    literal(7), interpolate(
-                        linear(), get("mag"),
-                        stop(1, 1),
-                        stop(6, 4)
-                    ),
-                    literal(16), interpolate(
-                        linear(), get("mag"),
-                        stop(1, 5),
-                        stop(6, 50)
+                circleRadius(
+                    interpolate(
+                        linear(), zoom(),
+                        literal(7), interpolate(
+                            linear(), get("mag"),
+                            stop(1, 1),
+                            stop(6, 4)
+                        ),
+                        literal(16), interpolate(
+                            linear(), get("mag"),
+                            stop(1, 5),
+                            stop(6, 50)
+                        )
                     )
-                )
-            ),
+                ),
 
 // Color circle by earthquake magnitude
-            circleColor(
-                interpolate(
-                    linear(), get("mag"),
-                    literal(1), rgba(33, 102, 172, 0),
-                    literal(2), rgb(103, 169, 207),
-                    literal(3), rgb(209, 229, 240),
-                    literal(4), rgb(253, 219, 199),
-                    literal(5), rgb(239, 138, 98),
-                    literal(6), rgb(178, 24, 43)
-                )
-            ),
+                circleColor(
+                    interpolate(
+                        linear(), get("mag"),
+                        literal(1), rgba(33, 102, 172, 0),
+                        literal(2), rgb(103, 169, 207),
+                        literal(3), rgb(209, 229, 240),
+                        literal(4), rgb(253, 219, 199),
+                        literal(5), rgb(239, 138, 98),
+                        literal(6), rgb(178, 24, 43)
+                    )
+                ),
 
 // Transition from heatmap to circle layer by zoom level
-            circleOpacity(
-                interpolate(
-                    linear(), zoom(),
-                    stop(7, 0),
-                    stop(8, 1)
-                )
-            ),
-            circleStrokeColor("white"),
-            circleStrokeWidth(1.0f)
-        )
+                circleOpacity(
+                    interpolate(
+                        linear(), zoom(),
+                        stop(7, 0),
+                        stop(8, 1)
+                    )
+                ),
+                circleStrokeColor("white"),
+                circleStrokeWidth(1.0f)
+            )
 
-        loadedMapStyle.addLayerBelow(circleLayer, HEATMAP_LAYER_ID)
+            loadedMapStyle.addLayerBelow(circleLayer, HEATMAP_LAYER_ID)
+        }
     }
 }
